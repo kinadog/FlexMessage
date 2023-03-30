eb9e7f820cca5dc243dd85ec9499a9c369dd0ec6
kinadog <65751092+kinadog@users.noreply.github.com>
2023-03-30 17:10:54 +0900

    Update Program.cs

--- src/FlexMessage/Program.cs
+++ src/FlexMessage/Program.cs
@@ -1,13 +1,16 @@
-﻿
-
-/*var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFlexMessage(builder, option => // ← 추가 (Add)
 {
-    option.FileMessageStatus = FileMessageStatus.LiveView; // 파일타입 메세지의 라이브뷰 보기여부
+    // 파일타입 메세지의 라이브뷰 보기여부 옵션
+    // Option for live view of file type messages.
+    option.FileMessageStatus = FileMessageStatus.LiveView; 
 }, message =>
 {
     // 여기에 데이터베이스에 메시지를 저장하는 코드를 작성하세요.
+    // DbMessage를 사용하지 않을 경우 아래의 코드는 삭제 하셔도 됩니다.
+    // Write code here to save the message to the database.
+    // If you are not using DbMessage, you can delete the following code.
 });
 
 builder.Services.AddControllersWithViews();
@@ -24,13 +27,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseFlexMessage(); // ← 추가 (Add)
+
 app.MapControllerRoute(
     "default",
     "{controller=Home}/{action=Index}/{id?}");
 
-// ← 샘플 페이지용 라우트를 추가합니다. (Add route for sample page)
-app.MapControllerRoute(
-    "sample",
-    "msg/controller=Sample/{action=Index}/{id?}");
-
-app.Run();*/
+app.Run();
