26e35607fd6b8cc5fe8f0f2712b549e56c665c51
Kinadog <65751092+kinadog@users.noreply.github.com>
2023-04-01 11:37:55 +0900

    Update flexMessage.js

--- src/FlexMessage/wwwroot/js/flexMessage/flexMessage.js
+++ src/FlexMessage/wwwroot/js/flexMessage/flexMessage.js
@@ -6,22 +6,49 @@ document.addEventListener("DOMContentLoaded", async () => {
     const host = window.location.hostname;
     const port = window.location.port;
 
-    // 서버와 웹소켓 연결 생성
-    const webSocket = new WebSocket(protocol + '://' + host + ':' + port +'/ws');
+    let webSocket;
+    let reconnectInterval;
+    let inactivityTimeout;
+    let reconnectAttempts = 0;
+    const maxReconnectAttempts = 3;
 
-    // 연결이 열리면 호출되는 이벤트 리스너
-    webSocket.addEventListener('open', (event) => {
-        console.log('WebSocket connection opened');
-    });
+    function resetInactivityTimeout() {
+        clearTimeout(inactivityTimeout);
+        inactivityTimeout = setTimeout(() => {
+            console.log("WebSocket inactive for too long, reconnecting...");
+            reconnectWebSocket();
+        }, 120000);
+        // 2분 동안 웹소켓이 사용되지 않으면 재접속 시도
+        // Attempt to reconnect if WebSocket is not used for 2 minutes
+    }
 
-    // 서버로부터 메시지를 받으면 호출되는 이벤트 리스너
-    webSocket.addEventListener('message', (event) => {
-        let data = JSON.parse(event.data);
-        let {Message: message, MsgType: msgType, IsId: isId} = data;
-        if(isId){
-            createCookie("webSocketId",message,1);
-        }else{
+    function connectWebSocket(){
+        // 서버와 웹소켓 연결 생성
+        // Create WebSocket connection to the server
+        const serverUrl = protocol + "://" + host + ":" + port + "/ws";
+        webSocket = new WebSocket(serverUrl);
+
+        // 연결이 열리면 호출되는 이벤트 리스너
+        // Event listener called when the connection opens
+        const onOpen = () => {
+            console.log("WebSocket connection opened");
+            reconnectInterval = null;
+            reconnectAttempts = 0;
+            clearInterval(reconnectInterval);
+            resetInactivityTimeout();
+        };
+
+        // 서버로부터 메시지를 받으면 호출되는 이벤트 리스너
+        // Event listener called when a message is received from the server
+        const onMessage = (event) => {
+            let data = JSON.parse(event.data);
+            let {Message: message, MsgType: msgType} = data;
             switch (msgType) {
+                case "Json": {
+                    message = JSON.parse(message);
+                    setFlexData(message);
+                    break;
+                }
                 case "BrowserConsole": {
                     // 메세지를 브라우저 콘솔에 출력합니다.
                     // Output the message to the browser console
@@ -67,85 +94,113 @@ document.addEventListener("DOMContentLoaded", async () => {
                     logViewer.textContent += message;
                     break;
                 }
-                case "Db": {
-                    /*
-                        DB에 기록 된 메세지를 실시간으로 웹에 출력하기 위한 샘플 코드 입니다.
-                        해당 기능이 필요 없으시면 이 코드를 삭제 하시면 됩니다.
-                        This is a sample code to output messages written to a DB in real time to the web.
-                        If you don't need this functionality, you can delete this code.
-                    */
-                    let dbViewer = document.getElementById('Db');
-                    let tableData = JSON.parse(message);
-                    const {Id, Message, Writedates} = tableData;
-
-                    let tdId = document.createElement('td');
-                    let tdMessage = document.createElement('td');
-                    let tdWritedate = document.createElement('td');
-                    tdId.textContent = Id;
-                    tdMessage.textContent = Message;
-                    tdWritedate.textContent = Writedates;
-
-                    let newTr = document.createElement('tr');
-                    newTr.appendChild(tdId);
-                    newTr.appendChild(tdMessage);
-                    newTr.appendChild(tdWritedate);
-
-                    let table = dbViewer.querySelector('table');
-                    let tbody = table.querySelector('tbody');
-                    if (!tbody) {
-                        tbody = document.createElement('tbody');
-                        table.appendChild(tbody);
-                    }
-                    tbody.appendChild(newTr);
-                    table.appendChild(tbody)
-                    break;
-                }
                 default: {
                     // 메세지를 브라우저 콘솔에 출력합니다.
                     // Output the message to the browser console
                     console.log(message);
                 }
             }
+            resetInactivityTimeout();
+        };
+
+        // 연결이 닫히면 호출되는 이벤트 리스너
+        // Event listener called when the connection closes
+        const onClose = () => {
+            console.log('WebSocket connection closed');
+            if (reconnectAttempts < maxReconnectAttempts) {
+                reconnectWebSocket();
+            } else {
+                removeAllListeners();
+            }
+        };
+
+        // 오류가 발생하면 호출되는 이벤트 리스너
+        // Event listener called when an error occurs
+        const onError = (event) => {
+            console.error('WebSocket error:', event);
+            if (reconnectAttempts < maxReconnectAttempts) {
+                reconnectWebSocket();
+            } else {
+                removeAllListeners();
+            }
+        };
+
+        // 이벤트 리스너 추가
+        // Add EventListener
+        webSocket.addEventListener("open", onOpen);
+        webSocket.addEventListener("message", onMessage);
+        webSocket.addEventListener("close", onClose);
+        webSocket.addEventListener("error", onError);
+
+        // 이벤트 리스너 제거
+        // remove EventListener
+        function removeAllListeners(){
+            webSocket.removeEventListener("open", onOpen);
+            webSocket.removeEventListener("message", onMessage);
+            webSocket.removeEventListener("close", onClose);
+            webSocket.removeEventListener("error", onError);
+            console.log('WebSocket Listener removed');
         }
-    });
+    }
 
-    // 연결이 닫히면 호출되는 이벤트 리스너
-    webSocket.addEventListener('close', (event) => {
-        console.log('WebSocket connection closed');
-    });
+    // 5초마다 재접속 시도
+    // Attempt to reconnect every 5 seconds
+    function reconnectWebSocket() {
+        if (reconnectInterval) return;
+        reconnectInterval = setInterval(() => {
+            reconnectAttempts += 1;
+            console.log("Attempting to reconnect...");
+            connectWebSocket();
+            if (reconnectAttempts >= maxReconnectAttempts) {
+                clearInterval(reconnectInterval);
+                reconnectInterval = null;
+            }
+        }, 5000);
+    }
 
-    // 오류가 발생하면 호출되는 이벤트 리스너
-    webSocket.addEventListener('error', (event) => {
-        console.error('WebSocket error:', event);
-    });
-
-
+    connectWebSocket();
 });
 
-// 쿠키를 생성합니다.
-// Create Cookie.
-function createCookie(name, value, days) {
-    let expires;
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    } else {
-        expires = "";
+class FlexEvent {
+    constructor() {
+        this.events = {};
     }
-    document.cookie = name + "=" + value + expires + "; path=/; secure";
-    return Promise.resolve();
+
+    subscribe(eventType, callback) {
+        if (!this.events[eventType]) {
+            this.events[eventType] = [];
+        }
+        this.events[eventType].push(callback);
+    }
+
+    publish(eventType, data) {
+        if (this.events[eventType]) {
+            this.events[eventType].forEach(callback => callback(data));
+        }
+    }
 }
 
-// 쿠키를 읽어옵니다.
-// Read Cookie.
-function readCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+const flexEvent = new FlexEvent();
+
+// 송신 된 Json값 저장
+//Save transmitted JSON value
+function setFlexData(value) {
+    window.flexData = value;
+    flexEvent.publish('flexData', value);
 }
+
+// 값 변경 시 원하는 작업 수행
+// Perform desired actions when the value changes
+function onFlexDataChanged(value) {
+    console.log(value);
+}
+
+// Json값 저장용 변수 초기화
+// Initialize variable for storing JSON value
+function clearFlexData() {
+    window.flexData = null;
+}
+
+// 이벤트 구독
+//Subscribe events
+flexEvent.subscribe('flexData', onFlexDataChanged);
